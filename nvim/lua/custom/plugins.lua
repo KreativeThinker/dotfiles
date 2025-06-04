d213aefdf7123858c6654af36919600839718b54
KreativeThinker <sagely2005eagle@gmail.com>
2025-06-04 18:32:18 +0530

    fix(neovim): switch to none-ls from null-ls
    BREAKING CHANGE: switch from null-ls to none-ls

--- nvim/lua/custom/plugins.lua
+++ nvim/lua/custom/plugins.lua
@@ -9,11 +9,15 @@ local plugins = {
     "neovim/nvim-lspconfig",
     dependencies = {
       -- format & linting
+      -- {
+      --   "jose-elias-alvarez/null-ls.nvim",
+      --   config = function()
+      --     require "custom.configs.null-ls"
+      --   end,
+      -- },
       {
-        "jose-elias-alvarez/null-ls.nvim",
-        config = function()
-          require "custom.configs.null-ls"
-        end,
+        "nvimtools/none-ls.nvim",
+        opts = {},
       },
     },
     config = function()
